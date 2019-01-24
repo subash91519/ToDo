a03ce8602c05e33525878a155d4df04e92769912
Subhash Dasari <subhashdasari@subhashs-macbook-air.local>
2019-01-24 20:12:00 +0530

    finished tableview delegate datasource methods

---
+++ ToDo/ToDoListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Subhash Dasari on 24/01/19.
+//  Copyright © 2019 Subhash Dasari. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    var content = ["BRN","Udemy","Varnith"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return content.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
+        cell.textLabel?.text = content[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+             tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+    }
+
+
+}
+
